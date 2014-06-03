4d938adb836f2150ad03066c947c51e9b75c17f9
Csati <csaba.barsony@gmail.com>
2014-06-03 23:02:28 +0200

    fix calendar directive data binding

--- public/scripts/fit_calendar.js
+++ public/scripts/fit_calendar.js
@@ -9,7 +9,7 @@ fit.directive("calendar", ["calendarService", function(calendarService){
 		templateUrl: "templates/fit_calendar.html",
 
 		scope: {
-			selectedMonth: "="
+			month: "="
 		},
 
 		link: function(scope){
@@ -17,9 +17,48 @@ fit.directive("calendar", ["calendarService", function(calendarService){
 				dayShortNames: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
 
 				clickOnDay: function(day){
-					console.log(scope.selectedMonth);
+					console.log(scope.month);
 				}
 			};
 		}
 	};
 }]);
+
+fit.factory("calendarService", function(){
+	return {
+		getFirstDay: function(year, month){
+			var firstDay = new Date(year, month, 1);
+
+			return convertDayNameToNumber(firstDay.toString().substring(0, 3));
+		},
+		getLastDay: function(year, month){
+			var lastDay = new Date(year, month + 1, 0);
+
+			return convertDayNameToNumber(lastDay.toString().substring(0, 3));
+		}
+	};
+
+	function convertDayNameToNumber(day){
+		if(day === "Mon"){
+			return 0;
+		}
+		else if(day === "Tue"){
+			return 1;
+		}
+		else if(day === "Wed"){
+			return 2;
+		}
+		else if(day === "Thu"){
+			return 3;
+		}
+		else if(day === "Fri"){
+			return 4;
+		}
+		else if(day === "Sat"){
+			return 5;
+		}
+		else{
+			return 6;
+		}
+	}
+});
